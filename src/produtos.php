7e56f3b91948d8198bbd68c107d52dbfdcdd2dea
Rodrigo Cabral <rodrigoalcab@gmail.com>
2019-07-25 21:52:32 -0400

    listagem de produtos finalizada

--- src/produtos.php
+++ src/produtos.php
@@ -1,4 +1,16 @@
 <?php require_once 'cabecalho.php' ?>
+<?php require_once 'global.php'; ?>
+<?php
+
+try{
+    $lista = Produto::listar();
+}catch (Exception $e){
+    Erro::trataErro();
+}
+
+
+?>
+
 <div class="row">
     <div class="col-md-12">
         <h2>Produtos</h2>
@@ -13,6 +25,7 @@
 
 <div class="row">
     <div class="col-md-12">
+        <?php if(count($lista) >= 0) {?>
         <table class="table">
             <thead>
             <tr>
@@ -26,17 +39,22 @@
             </tr>
             </thead>
             <tbody>
+                <?php foreach ($lista as $linha) { ?>
                 <tr>
-                    <td>1</td>
-                    <td>O Senhor dos Aneis</td>
-                    <td>R$ 80,55</td>
-                    <td>2</td>
-                    <td>Livros</td>
-                    <td><a href="/produtos-editar.php" class="btn btn-info">Editar</a></td>
-                    <td><a href="#" class="btn btn-danger">Excluir</a></td>
+                    <td><?php echo $linha['id'] ?></td>
+                    <td><?php echo $linha['nome'] ?></td>
+                    <td><?php echo $linha['preco'] ?></td>
+                    <td><?php echo $linha['quantidade'] ?></td>
+                    <td><?php echo $linha['categoria_nome'] ?></td>
+                    <td><a href="/produtos-editar.php?=<?php echo $linha['id'] ?>" class="btn btn-info">Editar</a></td>
+                    <td><a href="produtos-excluir-post.php?id=<?php echo $linha['id'] ?>" class="btn btn-danger">Excluir</a></td>
                 </tr>
+                <?php } ?>
             </tbody>
         </table>
+        <?php }else{
+            echo "<p>Nenhum produto cadastrado";
+        }?>
     </div>
 </div>
 <?php require_once 'rodape.php' ?>
